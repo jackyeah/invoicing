b5853ec4dcf108b49ec7fb9787d85d1773d5cf8e
jacky <jacky.yeh@cchntek.com>
2018-01-10 18:17:29 +0800

    [fix] update

--- js/purchase_new.js
+++ js/purchase_new.js
@@ -5,9 +5,8 @@ $(function() {
     });
 
     $("#nowPTitle").text('我要進貨').after("<li>進貨新產品</li>");
-    $('#subTitle').text('進貨新產品');
 
-    console.log($.cookie('invoicing_token'));
+    //console.log($.cookie('invoicing_token'));
 
     getManufacturers();
 
@@ -15,7 +14,9 @@ $(function() {
 });
 
 function getManufacturers(){
-    $.ajax({
+    $('#Manufacturers').append($('<option>', {value: 1, text: 'NIKE'}));
+    $('#Manufacturers').append($('<option>', {value: 2, text: 'ADIDAS'}));
+    /*$.ajax({
         url: apiUrl + '/backend/manufacturers',
         method: 'GET',
         xhrFields: {
@@ -31,7 +32,7 @@ function getManufacturers(){
                 var result = data.result;
                 if (parseInt(result.length) > 0) {
                     for (var i = 0; i < parseInt(result.length); i++) {
-                        $('#Ｍanufacturers').append($('<option>', {
+                        $('#Manufacturers').append($('<option>', {
                             value: result[i].id,
                             text: result[i].name
                         }));
@@ -43,7 +44,70 @@ function getManufacturers(){
             console.log(data);
             modal_msg('服務異常，請再度嘗試，若多次出現請聯繫管理員。');
         }
+    });*/
+}
+
+function addCount() {
+    $('#div_ColorCount').append('<div class="form-group"> <label>顏色款式</label> <input type="text" class="form-control" name="other" placeholder="顏色款式" value=""> </div> <div class="form-group"> <label>數量</label> <input type="text" class="form-control" name="quantity" placeholder="數量" value=""> </div>');
+}
+
+function saveData() {
+    var msg = '';
+    var Date = $('#Date').val();
+    var Manufacturers = $('#Manufacturers').val();
+    var Remark = $('#Remark').val();
+    var itemName = $('#itemName').val();
+    var itemNo = $('#itemNo').val();
+    var coast = $('#coast').val();
+    var price = $('#price').val();
+
+    if(Date == ''){
+        msg += "日期，請勿為空。<br />";
+    }
+
+    if(Manufacturers == '0'){
+        msg += "廠商，請勿為空。<br />";
+    }
+
+    if(itemName == ''){
+        msg += "商品名稱，請勿為空。<br />";
+    }
+
+    if(itemNo == ''){
+        msg += "商品貨號，請勿為空。<br />";
+    }
+
+    if(coast == ''){
+        msg += "成本，請勿為空。<br />";
+    }
+
+    if(price == ''){
+        msg += "售價，請勿為空。<br />";
+    }
+
+    var other = [];
+    var i = 1;
+    $("input[name='other']").each(function() {
+        if($(this).val() == ''){
+            msg += "顏色款式(" + i +  ")，請勿為空。<br />";
+        }
+        other.push($(this).val());
+        i++;
     });
 
+    var quantity = [];
+    var j = 1;
+    $("input[name='quantity']").each(function() {
+        if($(this).val() == ''){
+            msg += "數量(" + j +  ")，請勿為空。<br />";
+        }
+        quantity.push($(this).val());
+        j++;
+    });
 
+    if(msg == ''){
+        modal_btn('OK');
+    }else{
+        modal_btn(msg);
+    }
 }
