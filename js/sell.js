0dd7c50e0174e31cc1534a4a4c3bab748cb9cc0b
jacky <jacky.yeh@cchntek.com>
2018-01-24 18:14:47 +0800

    update

--- js/sell.js
+++ js/sell.js
@@ -15,84 +15,144 @@ $(function() {
 
     getShippingMethod();
 
+    getInventoryList();
+
     UnLoading();
 });
 
+/**
+ * 取得訂單來源清單
+ */
 function getOrderSource(){
-    $('#orderSource').append($('<option>', {value: 1, text: '蝦皮'}));
-    $('#orderSource').append($('<option>', {value: 2, text: '露天'}));
-    /*$.ajax({
-     url: apiUrl + '/backend/manufacturers',
-     method: 'GET',
-     xhrFields: {
-     withCredentials: true
-     },
-     headers: {
-     'Api-Token': $.cookie('invoicing_token')
-     },
-     dataType: 'json',
-     async: false,
-     success: function (data) {
-     if (data.error_code == '1') {
-     var result = data.result;
-     if (parseInt(result.length) > 0) {
-     for (var i = 0; i < parseInt(result.length); i++) {
-     $('#Manufacturers').append($('<option>', {
-     value: result[i].id,
-     text: result[i].name
-     }));
-     }
-     }
-     }
-     },
-     error: function (data) {
-     console.log(data);
-     modal_msg('服務異常，請再度嘗試，若多次出現請聯繫管理員。');
-     }
-     });*/
+    Loading();
+    $.ajax({
+        url: apiUrl + '/setting/order_source',
+        method: 'GET',
+        xhrFields: {
+            withCredentials: true
+        },
+        headers: {
+            'Api-Token': $.cookie('invoicing_token')
+        },
+        dataType: 'json',
+        async: false,
+        success: function (data) {
+            if(data.error_code == '1'){
+                var result = data.result;
+                var resultData = '';
+                for (var i = 0; i < parseInt(result.length); i++) {
+                    $('#orderSource').append($('<option>', {
+                        value: result[i].id,
+                        text: result[i].name
+                    }));
+                }
+
+                $('#order_source').html(resultData);
+            }
+
+            UnLoading();
+        },
+        error: function (data) {
+            console.log(data);
+            modal_msg('服務異常，請再度嘗試，若多次出現請聯繫管理員。');
+            UnLoading();
+        }
+    });
 }
 
+/**
+ * 取得寄送方式清單
+ */
 function getShippingMethod(){
-    $('#Transport').append($('<option>', {value: 1, text: '全家-貨到付款'}));
-    $('#Transport').append($('<option>', {value: 2, text: '7-11-貨到付款'}));
-    /*$.ajax({
-     url: apiUrl + '/backend/manufacturers',
-     method: 'GET',
-     xhrFields: {
-     withCredentials: true
-     },
-     headers: {
-     'Api-Token': $.cookie('invoicing_token')
-     },
-     dataType: 'json',
-     async: false,
-     success: function (data) {
-     if (data.error_code == '1') {
-     var result = data.result;
-     if (parseInt(result.length) > 0) {
-     for (var i = 0; i < parseInt(result.length); i++) {
-     $('#Manufacturers').append($('<option>', {
-     value: result[i].id,
-     text: result[i].name
-     }));
-     }
-     }
-     }
-     },
-     error: function (data) {
-     console.log(data);
-     modal_msg('服務異常，請再度嘗試，若多次出現請聯繫管理員。');
-     }
-     });*/
+    Loading();
+    $.ajax({
+        url: apiUrl + '/setting/shipping_method',
+        method: 'GET',
+        xhrFields: {
+            withCredentials: true
+        },
+        headers: {
+            'Api-Token': $.cookie('invoicing_token')
+        },
+        dataType: 'json',
+        async: false,
+        success: function (data) {
+            if(data.error_code == '1'){
+                var result = data.result;
+                var resultData = '';
+                for (var i = 0; i < parseInt(result.length); i++) {
+                    $('#Transport').append($('<option>', {
+                        value: result[i].id,
+                        text: result[i].name
+                    }));
+                }
+
+                $('#order_source').html(resultData);
+            }
+
+            UnLoading();
+        },
+        error: function (data) {
+            console.log(data);
+            modal_msg('服務異常，請再度嘗試，若多次出現請聯繫管理員。');
+            UnLoading();
+        }
+    });
 }
 
+/**
+ * 取得庫存清單
+ */
+function getInventoryList() {
+    Loading();
+    $.ajax({
+        url: apiUrl + '/inventory',
+        method: 'GET',
+        xhrFields: {
+            withCredentials: true
+        },
+        headers: {
+            'Api-Token': $.cookie('invoicing_token')
+        },
+        dataType: 'json',
+        async: false,
+        success: function (data) {
+            console.log(data);
+
+            if(data.error_code == '1'){
+                var result = data.result;
+                for (var i = 0; i < parseInt(result.length); i++) {
+                    $('#itemName').append($('<option>', {
+                        value: result[i].id,
+                        text: result[i].name + ' - ' + result[i].style + ' (' + result[i].item_no + ')',
+                        'data-coast': result[i].coast ,
+                        'data-price': result[i].price ,
+                        'data-quality': result[i].quality
+                    }));
+                }
+            }
+
+            UnLoading();
+        },
+        error: function (data) {
+            console.log(data);
+            modal_msg('服務異常，請再度嘗試，若多次出現請聯繫管理員。');
+            UnLoading();
+        }
+    });
+}
+
+/**
+ * 增加要出貨的產品
+ */
 function addProduct() {
     if ($('#itemName').val() != '') {
+        console.log($('#itemName option:selected')[0].dataset);
         var index = parseInt($('#productIndex').val()) + 1;
         var info = '<tr id="product' + index + '"> <td>' + $('#itemName option:selected').text() +
             '<input name="productID" type="hidden" value="' + $('#itemName option:selected').val() + '"></td> <td>' +
-            '<input type="text" class="form-control" name="productQuantity" id="productQuantity' + index + '" placeholder="數量" value=""></td> <td>' +
-            '<input type="text" class="form-control" name="productPrice" id="productPrice' + index + '" placeholder="售價" value="">' +
+            '<input type="text" class="form-control" name="productQuantity" id="productQuantity' + index + '" placeholder="庫存數量為：'+$('#itemName option:selected')[0].dataset.quality+'" value=""></td> <td>' +
+            '<input type="text" class="form-control" name="productPrice" id="productPrice' + index + '" placeholder="預計售價為：'+$('#itemName option:selected')[0].dataset.price+'" value="">' +
             '</td><td><span class="glyphicon glyphicon-remove" aria-hidden="true" style="float:right; color:red; cursor: pointer;" onclick="delProductDetail(' + index + ')"></span></td></tr>';
 
         $('#productInformation').append(info);
@@ -101,10 +161,17 @@ function addProduct() {
     }
 }
 
+/**
+ * 刪除要出貨的產品
+ * @param index
+ */
 function delProductDetail(index) {
     $('#product' + index).remove();
 }
 
+/**
+ * dialog，出貨
+ */
 function saveData() {
     var msg = '';
     var p_msg = false;
@@ -170,6 +237,9 @@ function saveData() {
     }
 }
 
+/**
+ * 出貨
+ */
 function modal_OK() {
     $('.modal-footer').hide();
     modal_btn('已成功儲存資料。');
